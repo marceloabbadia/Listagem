af45a355e872b869067a98cea66db67a7d1db85b
marceloabbadia <marcelo.abbadia@gmail.com>
2023-10-09 12:13:02 -0300

    adicionando texto e configuracao StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto1}>Nome do Evento</Text>
+      <Text style={styles.texto2}>sexta, 01 de dezmebro, 2023;</Text>
     </View>
   );
 }
@@ -13,8 +12,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: "#131016",
+  },
+  texto1: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  texto2: {
+    color: "#6b6b6b",
+    fontSize: 16,
   },
 });
